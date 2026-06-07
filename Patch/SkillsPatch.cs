5ff0778b908b291b133a7eef438889de278cb913
Black_Moss <kytheminecraft@outlook.com>
2026-06-08 01:18:28 +0800

    www

---
+++ Patch/SkillsPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace CustomFungamePack.Patch;
+
+[HarmonyPatch(typeof(Skills))]
+public class SkillsPatch
+{
+    [HarmonyPatch("Setup")]
+    [HarmonyPostfix]
+    private static void SetupPostfix(Skills __instance)
+    {
+        var xpData = FungameCheck.CurrentFungame.XpData;
+
+        __instance.INT = xpData.IntXp;
+        __instance.RES = xpData.ResXp;
+        __instance.STR = xpData.StrXp;
+        __instance.expINT = xpData.ExpInt;
+        __instance.expRES = xpData.ExpRes;
+        __instance.expSTR = xpData.ExpStr;
+
+        __instance.minINT = xpData.MinInt;
+        __instance.minRES = xpData.MinRes;
+        __instance.minSTR = xpData.MinStr;
+
+        __instance.maxINT = xpData.MaxInt;
+        __instance.maxRES = xpData.MaxRes;
+        __instance.maxSTR = xpData.MaxStr;
+    }
+}
--- Patch/WorldGenerationPatch.cs
+++ Patch/WorldGenerationPatch.cs
@@ -286,28 +286,6 @@ public static class WorldGenerationPatch
         }
     }
 
-    [HarmonyPatch(typeof(Skills), "Awake")]
-    private static void SetXp(Skills __instance)
-    {
-        var xpData = FungameCheck.CurrentFungame.XpData;
-
-        __instance.INT = xpData.IntXp;
-        __instance.RES = xpData.ResXp;
-        __instance.STR = xpData.StrXp;
-
-        __instance.expINT = xpData.ExpInt;
-        __instance.expRES = xpData.ExpRes;
-        __instance.expSTR = xpData.ExpStr;
-
-        __instance.minINT = xpData.MinInt;
-        __instance.minRES = xpData.MinRes;
-        __instance.minSTR = xpData.MinStr;
-
-        __instance.maxINT = xpData.MaxInt;
-        __instance.maxRES = xpData.MaxRes;
-        __instance.maxSTR = xpData.MaxStr;
-    }
-
     private static void ApplySettingsOverrides(Dictionary<string, object> overrides)
     {
         Settings.EnsureLoaded();
